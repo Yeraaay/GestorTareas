d87412083ea1fbd50d6de37a6007d41f14023074
jesguivil <ardijesus@gmail.com>
2023-09-21 17:02:41 +0200

    v2

--- gestorTareas/src/gestorTareas/tarea.java
+++ gestorTareas/src/gestorTareas/tarea.java
@@ -3,7 +3,7 @@ package gestorTareas;
 import java.util.Date;
 
 public class Tarea {
-//asdasdhpasd
+
 	protected String titulo;
 	protected String descripcion;
 	protected Date fecha;
@@ -25,13 +25,18 @@ public class Tarea {
 	public void setFecha(Date fecha) {
 		this.fecha = fecha;
 	}
-	@Override
+
+	
+	public Tarea(String titulo, String descripcion, Date fecha) {
+		super();
+		this.titulo = titulo;
+		this.descripcion = descripcion;
+		this.fecha = fecha;
+	}
+	
 	public String toString() {
 		return "Tarea [titulo=" + titulo + ", descripcion=" + descripcion + ", fecha=" + fecha + "]";
 	}
-	
-	
-
 
 
 }
